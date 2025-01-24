cac785a5ccaa5f51193dd015e67e831a78d25092
NikaMakharadze <nikamakharadze03@gmail.com>
2025-01-24 21:47:14 +0400

    Seventh commit

---
+++ level 36/Classwork/main.py
@@ -0,0 +1,14 @@
+# Level 36:
+
+'''
+1) https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/python
+2) https://www.codewars.com/kata/56b1f01c247c01db92000076/train/python
+3) https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/python
+4) https://www.codewars.com/kata/5aa736a455f906981800360d/train/python
+5) https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
+6) https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
+7) https://www.codewars.com/kata/563e320cee5dddcf77000158/train/python
+8) https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/python
+9) https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/python
+10) https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/python
+'''
---
+++ level 36/Homework/main.py
@@ -0,0 +1,14 @@
+# 1) ისწავლეთ args and kwargs ამ ვიდეოდან:
+# https://www.youtube.com/watch?v=4jBJhCaNrWU
+
+# თუ ამ ვიდეოდან კარგად ვერ გაიგებთ, შეგიძლიათ უყუროთ ამასაც:
+# https://www.youtube.com/watch?v=Vh__2V2tXUM
+
+# 2) ყველა ადით მინიმუმ 200 honor'ზე codewars'ში 
+'''
+3) https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
+4) https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/python
+5) https://www.codewars.com/kata/523b623152af8a30c6000027/train/python
+6) https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/python
+7) https://www.codewars.com/kata/5b853229cfde412a470000d0/train/python
+'''
