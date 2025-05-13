22b30c01982629d4337163b182c79a8b66fb06f5
NikaMakharadze <nikamakharadze03@gmail.com>
2025-05-13 08:03:09 -0700

    Fourteenth commit

---
+++ level 52/classwork/main.py
@@ -0,0 +1,10 @@
+'''
+5kyu:
+1) https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
+2) https://www.codewars.com/kata/52449b062fb80683ec000024/train/python
+3) https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/python
+
+4) https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+5) https://www.codewars.com/kata/550f22f4d758534c1100025a/train/python
+6) https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python
+'''
---
+++ level 52/homework/main.py
@@ -0,0 +1,7 @@
+'''
+5kyu:
+1) https://www.codewars.com/kata/525c65e51bf619685c000059/train/python
+2) https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/python
+3) https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/python
+4) https://www.codewars.com/kata/529b418d533b76924600085d/train/python
+'''
